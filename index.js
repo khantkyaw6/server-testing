eda566fef361cb1f5fbc2ca1e415c60590febaa1
Khant Kyaw <khantkankei@gmail.com>
2024-03-06 10:10:18 +0630

    login page added

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', (_req, res) => {
 	res.send('Hello from the server testing by KK');
 });
 
+app.get('/login', (_req, res) => {
+	res.send('This is login page');
+});
+
 app.listen(PORT, console.log('Server is running at PORT ', PORT));
